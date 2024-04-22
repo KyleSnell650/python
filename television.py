1d33dc86c63a5c5fc7e95b66ec49e1031e23f2ad
KyleSnell650 <kylesnell@unomaha.edu>
2024-04-21 21:10:00 -0500

    Final Rendition(hopefully)

--- television.py
+++ television.py
@@ -1,18 +1,43 @@
 class Television:
     MIN_VOLUME = 0
+    MIN_CHANNEL = 0
+    MAX_VOLUME = 2
+    MAX_CHANNEL = 3
 
     def __init__(self) -> None:
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
+        self.__status = False
+        self.__channel = Television.MIN_CHANNEL
 
     def power(self):
-        pass
+        '''
+        Method that turns the TV on/off
+        '''
+        if not self.__status:
+            self.__status = True
+        elif self.__status:
+            self.__status = False
 
     def mute(self) -> None:
-        pass
+        '''
+        Method that mutes the TV
+        '''
+        if self.__status:
+            if not self.__muted:
+                self.__muted = True
+            elif self.__muted:
+                self.__muted = False
 
     def channel_up(self):
-        pass
+        '''
+        Method to increase the tv channel
+        '''
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self):
         '''
@@ -29,9 +54,19 @@ class Television:
             self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
+            else:
+                self.__volume = Television.MIN_VOLUME
 
     def volume_down(self):
-        pass
+        '''
+        Method that turns the volume
+        '''
+        if self.__status:
+            self.__muted = False
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+            else:
+                self.__volume = Television.MAX_VOLUME
 
     def __str__(self) -> str:
         '''
@@ -39,8 +74,9 @@ class Television:
         :return: tv status.
         '''
         if self.__muted:
-            return f"Volume = {Television.MIN_VOLUME}"
+            volume = Television.MIN_VOLUME
         else:
-            return f"xxx"
+            volume = self.__volume
+        return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {volume}"
 
     
